59cba6f3edf97ee2b7aff2ead1312eb9e92d0b38
oiceo123 <iceome44@gmail.com>
2024-01-01 17:46:57 +0700

    ✨ import product, category container มาใช้

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,10 @@
 import "./Home.scss";
-import { Col, Row } from "antd";
 import { useTranslation } from "react-i18next";
+
+import { Col, Row } from "antd";
 import CarouselComponent from "../../components/Carousel";
-import CardGridComponent from "../../components/CardGrid";
-import CardCoverComponent from "../../components/CardCover";
+import ProductContainer from "../../containers/Product";
+import CategoryContainer from "../../containers/Category";
 
 const Home: React.FC = () => {
   const { t } = useTranslation();
@@ -27,11 +28,7 @@ const Home: React.FC = () => {
       </Row>
       <Row justify={"center"} style={{ marginTop: "24px" }}>
         <Col span={21}>
-          <CardGridComponent
-            title={t("page.header.home.title.category")}
-            bordered={true}
-            style={{ width: "10%", height: "151px", textAlign: "center" }}
-          />
+          <CategoryContainer />
         </Col>
       </Row>
       <Row justify={"center"} style={{ marginTop: "24px" }}>
@@ -42,84 +39,7 @@ const Home: React.FC = () => {
             </Col>
           </Row>
           <Row gutter={[16, 16]} style={{ marginTop: "24px" }}>
-            <Col span={4}>
-              <CardCoverComponent
-                style={{ width: "186px", borderRadius: 0 }}
-                cover={
-                  <img
-                    alt="example"
-                    src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
-                    style={{ borderRadius: 0 }}
-                  />
-                }
-                title="Europe Street beat"
-              />
-            </Col>
-            <Col span={4}>
-              <CardCoverComponent
-                style={{ width: "186px", borderRadius: 0 }}
-                cover={
-                  <img
-                    alt="example"
-                    src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
-                    style={{ borderRadius: 0 }}
-                  />
-                }
-                title="Europe Street beat"
-              />
-            </Col>
-            <Col span={4}>
-              <CardCoverComponent
-                style={{ width: "186px", borderRadius: 0 }}
-                cover={
-                  <img
-                    alt="example"
-                    src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
-                    style={{ borderRadius: 0 }}
-                  />
-                }
-                title="Europe Street beat"
-              />
-            </Col>
-            <Col span={4}>
-              <CardCoverComponent
-                style={{ width: "186px", borderRadius: 0 }}
-                cover={
-                  <img
-                    alt="example"
-                    src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
-                    style={{ borderRadius: 0 }}
-                  />
-                }
-                title="Europe Street beat"
-              />
-            </Col>
-            <Col span={4}>
-              <CardCoverComponent
-                style={{ width: "186px", borderRadius: 0 }}
-                cover={
-                  <img
-                    alt="example"
-                    src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
-                    style={{ borderRadius: 0 }}
-                  />
-                }
-                title="Europe Street beat"
-              />
-            </Col>
-            <Col span={4}>
-              <CardCoverComponent
-                style={{ width: "186px", borderRadius: 0 }}
-                cover={
-                  <img
-                    alt="example"
-                    src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
-                    style={{ borderRadius: 0 }}
-                  />
-                }
-                title="Europe Street beat"
-              />
-            </Col>
+            <ProductContainer />
           </Row>
         </Col>
       </Row>
