ce4c8ac96ff11040505e485efc4b4d2d958f14e5
oiceo123 <iceome44@gmail.com>
2024-01-03 12:59:50 +0700

    ✨ ปิดคำสั่ง console ใน production

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { useEffect } from "react";
 import NavbarContainer from "./containers/Navbar";
 import Router from "./routes";
 
 function App() {
+  useEffect(() => {
+    if (import.meta.env.VITE_NODE_ENV === "production") {
+      console.log = () => {};
+      console.warn = () => {};
+      console.error = () => {};
+      console.debug = () => {};
+      console.table = () => {};
+    }
+  }, []);
+
   return (
     <>
       <NavbarContainer />
