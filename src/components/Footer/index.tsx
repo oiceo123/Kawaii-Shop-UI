8617ef32b39d2811c075061f955c9ef05b818f4c
oiceo123 <iceome44@gmail.com>
2024-01-05 01:59:27 +0700

    ✨ เพิ่มส่วน footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.scss";
+
+const FooterComponent: React.FC = () => {
+  return (
+    <div className="web-components-footer-container">
+      Kawaii Shop &#169;2024 Created by Matcha
+    </div>
+  );
+};
+
+export default FooterComponent;
