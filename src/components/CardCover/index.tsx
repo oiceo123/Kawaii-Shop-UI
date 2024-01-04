78085b7ea62f8b012a2e7a1c334baefa1cb4f8d7
oiceo123 <iceome44@gmail.com>
2024-01-04 19:14:26 +0700

    ✨ เพิ่ม path ไปหน้ารายละเอียดสินค้า

--- src/components/CardCover/index.tsx
+++ src/components/CardCover/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Card } from "antd";
 
 interface CardCoverProps {
@@ -14,24 +15,26 @@ const CardCoverComponent: React.FC<CardCoverProps> = (props) => {
   const { style, cover, title, description } = props;
 
   return (
-    <Card
-      hoverable
-      style={style}
-      cover={
-        <img
-          alt="example"
-          src={cover}
-          style={{
-            borderRadius: 0,
-            objectFit: "cover",
-            width: "100%",
-            height: "189px",
-          }}
-        />
-      }
-    >
-      <Meta title={title} description={description} />
-    </Card>
+    <Link to={"/detail"}>
+      <Card
+        hoverable
+        style={style}
+        cover={
+          <img
+            alt="example"
+            src={cover}
+            style={{
+              borderRadius: 0,
+              objectFit: "cover",
+              width: "100%",
+              height: "189px",
+            }}
+          />
+        }
+      >
+        <Meta title={title} description={description} />
+      </Card>
+    </Link>
   );
 };
 
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,17 +3,19 @@ import Home from "../pages/Home";
 import SignIn from "../pages/SignIn";
 import SignUp from "../pages/SignUp";
 /* import PrivateRoute from "./PrivateRoute"; */
+import ProductDetailContainer from "../containers/ProductDetail";
 
 const Router = () => {
   return (
-      <Switch>
-        <Route path="/" component={Home} exact />
-        <Route path="/signin" component={SignIn} exact />
-        <Route path={"/signup"} component={SignUp} exact />
-        {/* <PrivateRoute path="/signup" exact>
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/signin" component={SignIn} exact />
+      <Route path={"/signup"} component={SignUp} exact />
+      <Route path={"/detail"} component={ProductDetailContainer} exact />
+      {/* <PrivateRoute path="/signup" exact>
           <SignUp />
         </PrivateRoute> */}
-      </Switch>
+    </Switch>
   );
 };
 
